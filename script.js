44a5b0bce9f0aa7702f2c4fb982c780216417917
Breedlove <Robert Breedlove@faa.gov>
2018-10-17 13:06:55 -0500

    Black Box test

---
+++ script.js
@@ -0,0 +1,9 @@
+var canv = document.getElementById("canvasGame");
+var c = canv.getContext("2d");
+
+canv.width = 800;
+canv.height = 600;
+
+c.fillStyle = "black";
+c.fillRect(0,0,canv.width,canv.height);
+
