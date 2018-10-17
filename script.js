4b4d7bdf8c0cf5005470552873da735b0bfbe597
Breedlove <Robert Breedlove@faa.gov>
2018-10-17 14:11:05 -0500

    star bounce

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 var canv = document.getElementById("canvasGame");
 var c = canv.getContext("2d");
 
+const FPS = 3; // frames per second
+
 canv.width = 800;
 canv.height = 600;
 
-c.fillStyle = "red";
-c.fillRect(0,0,canv.width,canv.height);
+// set up the game loop
+setInterval(screen, 1000 / FPS);
+
+function screen() {
+
+    // draw space
+    c.fillStyle = "black";
+    c.fillRect(0,0,canv.width,canv.height);
+    
+
+    // draw star
+    c.fillStyle = "white";
+    var starcount = 10; // number of stars on screen
+    var i;
+    for (i = 0; i < starcount; i++) {
+        c.fillRect(Math.floor(Math.random() * canv.width) + 1, Math.floor(Math.random() * canv.height) + 1, 10, 10);
+    }
+}
 
