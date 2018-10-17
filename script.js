e4d58db5754c6a9ab20a03b570d13b7b62aaa029
Breedlove <Robert Breedlove@faa.gov>
2018-10-17 14:30:25 -0500

    commit stars

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var canv = document.getElementById("canvasGame");
 var c = canv.getContext("2d");
 
-const FPS = 3; // frames per second
+const FPS = 1; // frames per second
 
-canv.width = 800;
-canv.height = 600;
+canv.width = window.innerWidth;
+canv.height = window.innerHeight;
 
 // set up the game loop
 setInterval(screen, 1000 / FPS);
@@ -18,7 +18,7 @@ function screen() {
 
     // draw star
     c.fillStyle = "white";
-    var starcount = 10; // number of stars on screen
+    var starcount = Math.floor(Math.random() * 10) + 1; // number of stars on screen
     var i;
     for (i = 0; i < starcount; i++) {
         c.fillRect(Math.floor(Math.random() * canv.width) + 1, Math.floor(Math.random() * canv.height) + 1, 10, 10);
